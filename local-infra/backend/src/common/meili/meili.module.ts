3483486e80da80fd843df8faf826c8bb6f854e8b
jefferson leon <jeffersonleon12@gmail.com>
2025-08-29 01:40:42 -0500

    feat(common): add common s3 redis rabbitmq meili services

---
+++ local-infra/backend/src/common/meili/meili.module.ts
@@ -0,0 +1,6 @@
+import { Global, Module } from '@nestjs/common';
+import { MeiliService } from './meili.service';
+
+@Global()
+@Module({ providers: [MeiliService], exports: [MeiliService] })
+export class MeiliModule {}

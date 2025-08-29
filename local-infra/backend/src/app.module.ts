c05f4a80dd72284d32ee87c48ec72da8c13b540a
jefferson leon <jeffersonleon12@gmail.com>
2025-08-29 01:50:19 -0500

    feat(api): config prefix v1 to api and call all endpoints

---
+++ local-infra/backend/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { HealthModule } from './health/health.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { VideosModule } from './videos/videos.module';
+import { UploadsModule } from './uploads/uploads.module';
+import { S3Module } from './common/s3/s3.module';
+import { RedisModule } from './common/redis/redis.module';
+import { RabbitMQModule } from './common/rabbitmq/rabbitmq.module';
+import { MeiliModule } from './common/meili/meili.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    HealthModule,
+    AuthModule,
+    VideosModule,
+    UploadsModule,
+    S3Module,
+    RedisModule,
+    RabbitMQModule,
+    MeiliModule,
+  ],
+})
+export class AppModule {}
